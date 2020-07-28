5845a56db7c94eae21c87b8ed2d3ea9ab9bb19f8
Cristian Ixcamey <cristian@pickleai.com>
2020-07-28 15:35:52 -0600

    test(tile.component): add test for tight variable
    #1307

--- projects/cashmere/src/lib/tile/tile.component.spec.ts
+++ projects/cashmere/src/lib/tile/tile.component.spec.ts
@@ -1,7 +1,7 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {TileComponent} from './tile.component';
-import {TileModule} from './tile.module';
+import { TileComponent } from './tile.component';
+import { TileModule } from './tile.module';
 
 describe('TileComponent', () => {
     let component: TileComponent;
@@ -22,4 +22,16 @@ describe('TileComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    describe("_tight", () => {
+        it('should check _tight to be false after being created', () => {
+            expect(component._tight).toBeFalsy();
+        });
+
+        it('should set _tight to "true"', () => {
+            component.tight = true;
+            expect(component._tight).toBeTruthy();
+        });
+    });
+
 });
