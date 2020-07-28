b068a37e60dab3e2036f000a53532e3a5af0d5b9
Cristian Ixcamey <cristian@pickleai.com>
2020-07-28 16:57:24 -0600

    test(tab): add base test for varibale
    #627

--- projects/cashmere/src/lib/tabs/tab/tab.component.spec.ts
+++ projects/cashmere/src/lib/tabs/tab/tab.component.spec.ts
@@ -1,6 +1,6 @@
-import {TabComponent} from './tab.component';
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
+import { TabComponent } from './tab.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TabComponent', () => {
     let component: TabComponent;
@@ -20,5 +20,14 @@ describe('TabComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    describe("variables", () => {
+        const expected = "testing"
+        it('should set tabTitle to a the assigned string of "testing"', () => {
+            component.tabTitle = "testing";
+            expect(component.tabTitle).toBe(expected);
+        });
+
     });
 });
